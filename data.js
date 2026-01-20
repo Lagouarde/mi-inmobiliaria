065bbd6b7d62b0ad454d199887efab9dd047f79e
Lagouarde <juanibalbi4@gmail.com>
2026-01-20 10:21:20 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+window.BARRIOS = [
+  {
+    "id": 1768826304780,
+    "nombre": "Barrio Rincon Verde",
+    "descripcion": "Hermoso barrio en la zona de Los Cardales",
+    "imagenMapa": "RINCON VERDE.jpg",
+    "lotes": [
+      {
+        "numero": "8",
+        "metros": 200,
+        "precio": 200000,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 73.65000009536743,
+        "y": 15.343287254991514
+      }
+    ]
+  }
+];

344bdb270943166e071ac21aee78e5af3f6519d8
Lagouarde <juanibalbi4@gmail.com>
2026-01-20 10:03:38 -0300

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-const BARRIOS = [
-  {
-    "id": 1768826304780,
-    "nombre": "Barrio Rincon Verde",
-    "descripcion": "Hermoso barrio en la zona de Los Cardales",
-    "imagenMapa": "RINCON VERDE.jpg",
-    "lotes": []
-  }
-]

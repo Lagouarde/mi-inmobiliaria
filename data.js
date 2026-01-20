a8a49068d36236036709dc5cb816f912a16d448d
Lagouarde <juanibalbi4@gmail.com>
2026-01-20 12:18:40 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,540 @@
+window.BARRIOS = [
+  {
+    "id": 1768920253761,
+    "nombre": "Barrio Rincon Verde",
+    "descripcion": "Excelente ubicacion sobre Ruta 6 a 300 metros del centro",
+    "imagenMapa": "RINCON VERDE.jpg",
+    "lotes": [
+      {
+        "numero": "A",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 5.400000095367432,
+        "y": 8.50774564114753
+      },
+      {
+        "numero": "B",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 12.775000095367432,
+        "y": 12.029085320261649
+      },
+      {
+        "numero": "C",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 16.52500009536743,
+        "y": 12.098130796074576
+      },
+      {
+        "numero": "F",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 28.52500009536743,
+        "y": 11.821947740495578
+      },
+      {
+        "numero": "K",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 36.77500009536743,
+        "y": 31.948818678838194
+      },
+      {
+        "numero": "O",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 16.77500009536743,
+        "y": 31.189314494059573
+      },
+      {
+        "numero": "N",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 20.52500009536743,
+        "y": 30.982176914293504
+      },
+      {
+        "numero": "L",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 28.650000095367435,
+        "y": 30.8095620724339
+      },
+      {
+        "numero": "AK",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 45.90000009536743,
+        "y": 31.465497714256756
+      },
+      {
+        "numero": "AJ",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 54.02500009536744,
+        "y": 31.01669981681815
+      },
+      {
+        "numero": "AI",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 57.77500009536743,
+        "y": 30.602424657286008
+      },
+      {
+        "numero": "AH",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 61.775000095367425,
+        "y": 30.464332717951052
+      },
+      {
+        "numero": "AG",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 66.02500009536743,
+        "y": 30.98217707891169
+      },
+      {
+        "numero": "AF",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 69.90000009536743,
+        "y": 30.18814949775387
+      },
+      {
+        "numero": "V",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 54.02500009536744,
+        "y": 11.40766616085425
+      },
+      {
+        "numero": "W",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 58.02500009536743,
+        "y": 11.960039515212353
+      },
+      {
+        "numero": "X",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 61.90000009536744,
+        "y": 11.476718344858186
+      },
+      {
+        "numero": "Y",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 65.65000009536743,
+        "y": 11.545764026443845
+      },
+      {
+        "numero": "Z",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 69.90000009536743,
+        "y": 11.92551677730589
+      },
+      {
+        "numero": "AV",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 46.52500009536743,
+        "y": 43.8592320669691
+      },
+      {
+        "numero": "T",
+        "metros": 305.97,
+        "precio": 30000,
+        "financiacion": "Hasta 24 cuotas (35000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 5.150000095367432,
+        "y": 14.445690801641566
+      },
+      {
+        "numero": "AQ",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 67.02500009536743,
+        "y": 42.85806641219066
+      },
+      {
+        "numero": "AW",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 48.65000009536743,
+        "y": 53.80183721268595
+      },
+      {
+        "numero": "AX",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 49.02500009536743,
+        "y": 59.39455236022441
+      },
+      {
+        "numero": "AS",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 58.650000095367425,
+        "y": 42.99615868076199
+      },
+      {
+        "numero": "AR",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 62.77500009536743,
+        "y": 43.03068108943208
+      },
+      {
+        "numero": "BB",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Reservado",
+        "x": 57.52500009536743,
+        "y": 72.09899300027676
+      },
+      {
+        "numero": "BC",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Reservado",
+        "x": 62.52500009536743,
+        "y": 68.09433301505395
+      },
+      {
+        "numero": "BD",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Reservado",
+        "x": 67.40000009536743,
+        "y": 65.5396352651027
+      },
+      {
+        "numero": "BH",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Reservado",
+        "x": 86.77500009536743,
+        "y": 51.35070932263594
+      },
+      {
+        "numero": "BI",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Reservado",
+        "x": 91.27500009536743,
+        "y": 48.692444346674414
+      },
+      {
+        "numero": "S",
+        "metros": 306.07,
+        "precio": 30000,
+        "financiacion": "Hasta 24 cuotas (35000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 5.400000095367432,
+        "y": 20.07292918094104
+      },
+      {
+        "numero": "R",
+        "metros": 306.17,
+        "precio": 30000,
+        "financiacion": "Hasta 24 cuotas (35000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 5.150000095367432,
+        "y": 26.183488854058318
+      },
+      {
+        "numero": "Q",
+        "metros": 301.79,
+        "precio": 30000,
+        "financiacion": "Hasta 24 cuotas (35000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 5.650000095367432,
+        "y": 31.948819008074565
+      },
+      {
+        "numero": "D",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 20.52500009536743,
+        "y": 12.098130137601839
+      },
+      {
+        "numero": "E",
+        "metros": 375,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 24.52500009536743,
+        "y": 11.718378374448898
+      },
+      {
+        "numero": "M",
+        "metros": 377.24,
+        "precio": 30000,
+        "financiacion": "Hasta 24 cuotas (35000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 24.65000009536743,
+        "y": 30.878608124410466
+      },
+      {
+        "numero": "P",
+        "metros": 376.96,
+        "precio": 30000,
+        "financiacion": "Hasta 24 cuotas (35000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 12.650000095367432,
+        "y": 31.327406268776347
+      },
+      {
+        "numero": "G",
+        "metros": 307.51,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 36.65000009536743,
+        "y": 8.922021129916038
+      },
+      {
+        "numero": "H",
+        "metros": 310.04,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 37.02500009536743,
+        "y": 14.61830547888299
+      },
+      {
+        "numero": "I",
+        "metros": 310.04,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 36.77500009536743,
+        "y": 20.21102128489418
+      },
+      {
+        "numero": "J",
+        "metros": 310.04,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 36.90000009536743,
+        "y": 25.941826725585752
+      },
+      {
+        "numero": "U",
+        "metros": 305.97,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 46.52500009536743,
+        "y": 9.577956936357083
+      },
+      {
+        "numero": "AN",
+        "metros": 310,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 UISD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 46.02500009536743,
+        "y": 14.790920156124413
+      },
+      {
+        "numero": "AM",
+        "metros": 310,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 45.77500009536743,
+        "y": 20.625295456717218
+      },
+      {
+        "numero": "AL",
+        "metros": 310,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 46.02500009536743,
+        "y": 26.459672732728233
+      },
+      {
+        "numero": "AA",
+        "metros": null,
+        "precio": null,
+        "financiacion": null,
+        "imagen": null,
+        "estado": "Vendido",
+        "x": 73.52500009536743,
+        "y": 15.619468499770484
+      },
+      {
+        "numero": "AE",
+        "metros": 315.74,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 73.77500009536743,
+        "y": 29.08341595849241
+      },
+      {
+        "numero": "AD",
+        "metros": 332.13,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 79.40000009536743,
+        "y": 26.183488854058318
+      },
+      {
+        "numero": "AU",
+        "metros": 301.36,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 50.77500009536743,
+        "y": 43.06520481504765
+      },
+      {
+        "numero": "AT",
+        "metros": 301.34,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 55.025000095367425,
+        "y": 43.13425014681967
+      },
+      {
+        "numero": "AP",
+        "metros": 376.17,
+        "precio": 25000,
+        "financiacion": "Hasta 24  cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 70.52500009536743,
+        "y": 42.754497869234896
+      },
+      {
+        "numero": "AO",
+        "metros": 429.48,
+        "precio": 25000,
+        "financiacion": "Hasta 24 cuotas (30000 USD)",
+        "imagen": null,
+        "estado": "Disponible",
+        "x": 74.52500009536743,
+        "y": 43.134250949333314
+      }
+    ]
+  }
+];
